ecd753e23090cdb66f4c26808210a4189504b1f3
Hortanio Rocha Silva <hortanio.silva@dotzmkt.com.br>
2022-12-06 15:40:07 -0300

    Correção de url api

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const BASE_API = 'https://barber-test-api.herokuapp.com/api';
+const BASE_API = 'https://barber-api-9hiu.onrender.com/api';
 
 export default new class Api {
 
